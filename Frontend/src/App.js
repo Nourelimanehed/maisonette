873dbb82e5328b7d5c1084beb6310ab8066f78e7
Meriem Soltani <82027047+chammariouma@users.noreply.github.com>
2023-01-08 20:30:58 +0100

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,30 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Help from "./components/Help";
+import Adds from "./components/Adds";
+import MyOffers from "./components/MyOffers";
+import MyAdds from "./components/MyAdds";
+import NoPage from "./components/NoPage"
+
+import './App.css'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+    return (
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header/>}>
+          <Route index element={<Home/>} />
+          <Route path="aide" element={<Help/>} />
+          <Route path="annonces" element={<Adds/>} />
+          <Route path="mesOffres" element={<MyOffers/>} />
+          <Route path="mesAnnonces" element={<MyAdds/>} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    );
+  }
+
+  export default App;
